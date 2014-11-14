b6c66bc60285c0fc4436048ade5c12b9d7d1b3a2
Adrián Mouly <adrian@mouly.com.ar>
2014-11-14 12:36:53 -0300

    Fixes.

--- program2.js
+++ program2.js
@@ -1,9 +1,10 @@
+/*globals console,process*/
 var argv = process.argv,
     i,
     sum;
 
-for(i = 2; i <= argv.length; i++){
-    sum += Number(argv[i]);
+for (i = 2; i <= argv.length; i += 1) {
+    sum += argv[i];
 }
 
 console.log(sum);
