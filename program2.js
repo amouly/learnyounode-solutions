24fcde30da527912d4fee14f3a2dc6092e87f75f
Adrián Mouly <adrian@mouly.com.ar>
2014-11-14 12:43:13 -0300

    Last fix.

--- program2.js
+++ program2.js
@@ -3,10 +3,8 @@ var argv = process.argv,
     i,
     sum = 0;
 
-for (i = 2; i <= argv.length; i += 1) {
+for (i = 2; i < argv.length; i += 1) {
     sum = sum + Number(argv[i]);
-
-    console.log(argv[i]);
 }
 
 console.log(sum);
