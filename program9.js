57f709d95368fa8a5ca895dd4bba98e50f497ce2
Adrián Mouly <adrian@mouly.com.ar>
2014-11-21 17:13:46 -0300

    Fixes on buffer.

--- program9.js
+++ program9.js
@@ -1,22 +1,16 @@
 /*globals console,process,require*/
-var bl = require('bl'),
+var BufferList = require('bl'),
     http = require('http'),
     urls = [process.argv[2], process.argv[3], process.argv[4]],
-    finalData = "";
+    buffer = new BufferList();
 
 urls.forEach(function (url, index) {
     'use strict';
 
     http.get(url, function done(response) {
 
-        response.pipe(bl(function (err, data) {
-            if (err) {
-                return console.error(err);
-            }
-
-            finalData += data;
-        }));
+        buffer.append(response);
     });
 });
 
-console.log(finalData);
+console.log(buffer.toString());
